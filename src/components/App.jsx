fe014ca18797038cf928c4163c360f3f7b41b134
wxnyeki <frankwanyeki01@gmail.com>
2026-05-14 23:32:47 -0700

    Fetch and manage plants in App component
    Fetch plant data from the server and manage state.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
+import { useState,useEffect } from "react";
 
 function App() {
+    const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(r => r.json())
+      .then(setPlants);
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants(prev => [...prev, newPlant]);
+  }
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants} onAddPlant={handleAddPlant}/>
     </div>
   );
 }
